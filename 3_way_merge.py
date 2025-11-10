2f2f8f4c5304e8144a837ab51538c912a0719554
iccvbenchmarkfordlidar <iccvbenchmarkfordlidar@gmail.com>
2025-11-11 01:44:42 +0900

    Learn Git & Github!

--- 3_way_merge.py
+++ 3_way_merge.py
@@ -1,7 +1,9 @@
+# Please remove the comments from the code below in terminal A
 # def A_part():
 #     # This should be done by A person
 #     pass
     
+# Please remove the comments from the code below in terminal B
 # def run():
 #     if A_part():
 #         print("It is okay!")
